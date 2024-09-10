9493914b851e9e706100810c834cde8e3930bc60
Dhirendra Singh <115912791+dhirendrasingh25@users.noreply.github.com>
2024-09-10 19:36:47 +0530

    bento Grid completed

--- portfolio/components/ui/background-gradient-animation.tsx
+++ portfolio/components/ui/background-gradient-animation.tsx
@@ -89,7 +89,7 @@ export const BackgroundGradientAnimation = ({
   return (
     <div
       className={cn(
-        "h-full w-full p-10 rounded-md relative overflow-hidden top-0 left-0 bg-[linear-gradient(40deg,var(--gradient-background-start),var(--gradient-background-end))]",
+        "h-full w-full p-10 rounded-md absolute overflow-hidden top-0 left-0 bg-[linear-gradient(40deg,var(--gradient-background-start),var(--gradient-background-end))]",
         containerClassName
       )}
     >
