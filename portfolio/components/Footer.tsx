c06584f73a0b63443243c90141119510d701a2e6
Dhirendra Singh <115912791+dhirendrasingh25@users.noreply.github.com>
2024-09-16 23:14:06 +0530

    Completed footer

--- portfolio/components/Footer.tsx
+++ portfolio/components/Footer.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="w-full pt-20 pb-20" id="contact">
+    <footer className="w-full pt-20 pb-10" id="contact">
       <div className="w-full absolute left-0 -bottom-72 min-h-96">
         <img
           src="/footer-grid.svg"
@@ -15,12 +15,10 @@ const Footer = () => {
       </div>
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
-          Ready to take <span className="text-purple">your</span> digital
-          presence to the next level?
+         Let's build<span className="text-purple"> your</span> vision and take it to new heights
         </h1>
         <p className="text-white-200 md:mt-10 my-5 text-center">
-          Reach out to me today and let&apos;s discuss how I can help you
-          achieve your goals.
+          Let’s team up today to create something amazing
         </p>
         <div className="flex flex-row ">
             <div className="flex px-2 items-center">
@@ -39,6 +37,7 @@ const Footer = () => {
               
               className="w-12 h-12 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-500"
             >
+              
               <img src={info.img} alt="icons" width={22} height={22} />
             </div>
             </Link>
@@ -50,22 +49,9 @@ const Footer = () => {
 
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
-        <p className="md:text-base text-sm md:font-normal font-light">
-          Copyright © 2024 Dhirendra Singh
+        <p className="text-xs  font-light text-white-200">
+          Copyright © 2024 Dhirendra Singh | Developed with NextJs & Aceternity UI | UI reference JS Mastery YT
         </p>
-
-        {/* <div className="flex items-center md:gap-3 gap-6">
-          {socialMedia.map((info) => (
-            <Link href={info.link} key={info.id}>
-            <div
-              
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
-            >
-              <img src={info.img} alt="icons" width={20} height={20} />
-            </div>
-            </Link>
-          ))}
-        </div> */}
       </div>
     </footer>
   );
